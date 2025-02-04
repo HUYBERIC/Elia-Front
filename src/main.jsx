c2fc01c59e78ab46e97acbe7347b72696cfb32cf
Antoine Lafontaine <antoinelaf@gmail.com>
2025-02-04 10:30:27 +0100

    jwt-decode, google auth ???

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./styles/main.scss";
 import App from "./App.jsx";
 
+const clientId =
+  "75767399198-g2f2n0vdvbcfbeo23mnovmsqgiuaimom.apps.googleusercontent.com"; // Your actual Client ID
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={clientId}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>
 );
