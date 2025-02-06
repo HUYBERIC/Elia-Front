76fcde6769ec44939baed572cd5e25f54428e69c
Antoine L <162304112+t0rbenC0rtes@users.noreply.github.com>
2025-02-06 14:56:26 +0100

    feat: add Notification component with styling and integrate into Alerts page

--- src/pages/Alerts.jsx
+++ src/pages/Alerts.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
 import SwitchRequestModal from "../components/SwitchRequestModal";
+import Notification from "../components/Notification";
 
 const Alerts = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -11,7 +12,7 @@ const Alerts = () => {
       <div className="alerts-container">
         {/* Opening modal button */}
         <button onClick={() => setIsModalOpen(true)}>Request a switch</button>
-        
+        <Notification />
         {/* Modal */}
         <SwitchRequestModal
           isOpen={isModalOpen}
