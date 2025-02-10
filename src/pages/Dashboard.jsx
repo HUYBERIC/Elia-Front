0813c27a1dc2fd4c39f44ddaab8fa651516d699d
MaximeB <maxounu@gmail.com>
2025-02-10 15:56:01 +0100

    feat: ajouter la récupération des demandes et remplacements dans le composant Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,72 @@
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 
 const Dashboard = () => {
+  const [requests, setRequests] = useState([]);
+  const [replacements, setReplacements] = useState([]);
+
+  useEffect(() => {
+    // Fonction pour récupérer les demandes
+    const fetchRequests = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/requests", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        });
+
+        if (!response.ok) throw new Error("Failed to fetch requests");
+
+        const data = await response.json();
+        setRequests(data);
+      } catch (error) {
+        console.error("Error fetching requests:", error);
+      }
+    };
+
+    // Fonction pour récupérer les remplacements
+    const fetchReplacements = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/replacements", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        });
+
+        if (!response.ok) throw new Error("Failed to fetch replacements");
+
+        const data = await response.json();
+        setReplacements(data);
+      } catch (error) {
+        console.error("Error fetching replacements:", error);
+      }
+    };
+
+    fetchRequests();
+    fetchReplacements();
+  }, []);
+
   return (
     <div className="feed-container">
       <div className="feed">
-        <p>User1 requested duty switch @day-month-year (status: accepted)</p>
-        <p>User5 accepted request from User1</p>
-        <p>User1's request has been completed</p>
-        <p>User3 requested duty switch @day-month-year (status: pending)</p>
+        
+
+        <h3>Replacements</h3>
+        {replacements.length === 0 ? (
+          <p>No replacements</p>
+        ) : (
+          replacements.slice().reverse().map((replacement) => (
+            <p key={replacement._id}>
+              <strong>{replacement.replacingUserId?.firstName}</strong> remplace
+              le shift de{" "}  
+              <strong>{replacement.replacedUserId?.firstName}</strong> le{" "}
+              {new Date(replacement.startTime).toLocaleDateString()} de{" "}
+              {new Date(replacement.startTime).toLocaleTimeString()} jusqu'au{" "}
+              {new Date(replacement.endTime).toLocaleDateString()} à{" "}
+              {new Date(replacement.endTime).toLocaleTimeString()}.
+            </p>
+          ))
+        )}
       </div>
       <Navbar />
     </div>
@@ -15,3 +74,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
