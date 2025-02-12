abdf3ae60c5a2111999137c8ceb47ac80cc54a7e
huyberic <huyberic@gmail.com>
2025-02-12 13:55:53 +0100

    refactor: improve Dashboard notifications layout and styling for better readability

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -48,35 +48,64 @@ const Dashboard = () => {
 
   return (
     <div className="feed-container">
-        <div className="title">
-          <h3>Replacements</h3>
-        </div>
-        <div className="notifs">
-          {replacements.length === 0 ? (
-            <p>No replacements</p>
-          ) : (
-            replacements
-              .slice()
-              .reverse()
-              .map(
-                (replacement) =>
-                  new Date() - new Date(replacement.startTime) <
-                    24 * 60 * 60 * 1000 && (
-                    <div key={replacement._id} className="notif">
-                      <p className="notif-content"><span className="user">{replacement.replacingUserId?.firstName}{" "}</span>
-                      is replacing the shift of{" "}
-                      <span className="user">{replacement.replacedUserId?.firstName}{" "}</span>
-                      on {new Date(replacement.startTime).toLocaleDateString()}{" "}
-                      from{" "}
-                      {new Date(replacement.startTime).toLocaleTimeString()} to{" "}
-                      {new Date(replacement.endTime).toLocaleDateString()} at{" "}
-                      {new Date(replacement.endTime).toLocaleTimeString()}.
-                      </p>
-                    </div>
-                  )
-              )
-          )}
-        </div>
+      <div className="title">
+        <h3>Replacements</h3>
+      </div>
+      <div className="notifs">
+        {replacements.length === 0 ? (
+          <p>No replacements</p>
+        ) : (
+          replacements
+            .slice()
+            .reverse()
+            .map(
+              (replacement) =>
+                new Date() - new Date(replacement.startTime) <
+                  24 * 60 * 60 * 1000 && (
+                  <div key={replacement._id} className="notif">
+                    <p className="notif-content">
+                      <span className="user">
+                        {replacement.replacingUserId?.firstName}
+                      </span>{" "}
+                      replaces{" "}
+                      <span className="user">
+                        {replacement.replacedUserId?.firstName}
+                      </span>{" "}
+                      <br></br>
+                      <span className="from-to">from</span> :{" "}
+                      <span className="bold">
+                        {new Date(replacement.startTime).toLocaleDateString(
+                          "fr-FR"
+                        )}
+                      </span>{" "}
+                      - {" "}
+                      <span className="bold">
+                        {new Date(replacement.startTime).toLocaleTimeString(
+                          "fr-FR",
+                          { hour: "2-digit", minute: "2-digit", hour12: false }
+                        )}
+                      </span>{" "}
+                      <br></br>
+                      <span className="from-to">to</span> :{" "}
+                      <span className="bold">
+                        {new Date(replacement.endTime).toLocaleDateString(
+                          "fr-FR"
+                        )}
+                      </span>{" "}
+                      - {" "}
+                      <span className="bold">
+                        {new Date(replacement.endTime).toLocaleTimeString(
+                          "fr-FR",
+                          { hour: "2-digit", minute: "2-digit", hour12: false }
+                        )}
+                      </span>
+                      .
+                    </p>
+                  </div>
+                )
+            )
+        )}
+      </div>
       <Navbar />
     </div>
   );
