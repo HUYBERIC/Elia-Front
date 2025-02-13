96ba12ea47cdbcc5a4bd1eecb1a3ad6dc8f715e8
huyberic <huyberic@gmail.com>
2025-02-13 08:48:47 +0100

    feat: set fixed server port and enforce strict port usage in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Définit un port fixe
+    strictPort: true, // Empêche Vite de changer de port si 3000 est occupé
+  }
 })
