3362e0aa898bcd69f1f6b158297d7ca209b00628
MaximeB <maxounu@gmail.com>
2025-02-17 10:17:21 +0100

    refactor: remove hardcoded client ID for security reasons

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles/main.scss";
 import App from "./App.jsx";
 
-const clientId =
-  "522967352271-54aub3k94o9kg41e45icivraj3oiqv7n.apps.googleusercontent.com"; // Your actual Client ID
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
